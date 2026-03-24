b06e9be86e3fcbb12145a2d447aa1721d17a45ae
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-03-24 11:34:43 -0600

    Modifications at main.c to add more explicit, verbose print
    - Add more information to print about node's name

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 int main(int argn, char **args)
 {
         int rank , size; // nombre del proceso y numero de procesos
-//      char hostname[];
+        int len; // numero de nodos
+        char hostname[MPI_MAX_PROCESSOR_NAME];
 
         MPI_Init(&argn,&args); // inicializar el entorno de mpi
 
         MPI_Comm_rank(MPI_COMM_WORLD, &rank); // numero de proceo
         MPI_Comm_size(MPI_COMM_WORLD, &size); // numero total de procesos
 
-        printf("HOla mundo soy el proceso %i de %i\n",rank,size);
+        MPI_Get_processor_name(hostname, &len); // recuperar nombre del proceso
+
+        printf("Hola mundo soy el proceso %i de %i en %s\n", rank, size, hostname);
 
         MPI_Finalize(); // finalizar el entorno de mpi
 
