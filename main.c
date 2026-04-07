cf2f4855b3521c843c50e95e0197a0bc6e966dbb
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-04-07 11:37:45 -0600

    Add send and recv sentences to communicate data

--- main.c
+++ main.c
@@ -7,7 +7,11 @@ int main(int argn, char **args)
         int rank , size; // nombre del proceso y numero de procesos
         int len; // numero de nodos
         char hostname[MPI_MAX_PROCESSOR_NAME];
+        MPI_Status status;
 
+        int result_i;
+        int result = 0;
+        
         MPI_Init(&argn,&args); // inicializar el entorno de mpi
 
         MPI_Barrier(MPI_COMM_WORLD);
@@ -19,8 +23,22 @@ int main(int argn, char **args)
 
         MPI_Barrier(MPI_COMM_WORLD);
         
-        int result = rank*rank;
-        printf("Hola mundo soy el proceso %i de %i en %s y mi resultado es: %i\n", rank, size, hostname, result);
+        // printf("Hola mundo soy el proceso %i de %i en %s y mi resultado es: %i\n", rank, size, hostname, result);
+
+        if (rank == 0) // nodo maestro
+        {
+                for (int i=1; i<size; i++)
+                {
+                        MPI_Recv(&result_i, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+                        result = result + result_i;
+                }
+                printf("Suma = %i\n", result);
+        }
+        else // nodos esclavos
+        {       
+                result_i = rank*rank;
+                MPI_Send(&result_i, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
 
         MPI_Barrier(MPI_COMM_WORLD);
 
